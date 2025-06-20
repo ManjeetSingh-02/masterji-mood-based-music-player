6de6071e238cd94c9a8bf14ac657248f44d7407e
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2025-06-21 02:16:00 +0530

    added shadcn and import aliases

--- src/routes/__root.jsx
+++ src/routes/__root.jsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Outlet, createRootRoute } from '@tanstack/react-router';
-import { Footer, Header } from '../components';
+import Header from '@/components/Header/Header';
+import Footer from '@/components/Footer/Footer';
 
 export const Route = createRootRoute({
   component: RootComponent,
